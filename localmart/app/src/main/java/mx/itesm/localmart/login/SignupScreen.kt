eff7532ef3c0fe1b3bd7fa7bf0a35f83104b24eb
Nat <nattaliamt@hotmial.com>
2020-04-22 14:33:57 -0500

    SignupScreen.kt y activity_signup_screen.xml

---
+++ localmart/app/src/main/java/mx/itesm/localmart/login/SignupScreen.kt
@@ -0,0 +1,13 @@
+package mx.itesm.localmart.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.itesm.localmart.R
+
+class SignupScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_screen)
+    }
+}
