107997bd5320611e3284f792cd75a6c5544db30b
Duncanista <30836115+duncanista@users.noreply.github.com>
2020-04-22 11:12:16 -0500

    Bottom Navigation refactored with fragments

--- localmart/app/src/main/java/mx/itesm/localmart/MainActivity.kt
+++ localmart/app/src/main/java/mx/itesm/localmart/MainActivity.kt
@@ -7,6 +7,9 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +18,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = bottom_navigation
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
 
     }
 }
