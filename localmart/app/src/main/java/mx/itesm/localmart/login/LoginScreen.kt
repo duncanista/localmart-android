9fee7357dd4b10f2020a2a5a6b136496bf997da8
Duncanista <30836115+duncanista@users.noreply.github.com>
2020-04-22 16:04:48 -0500

    Workflow on log in and sign up

--- localmart/app/src/main/java/mx/itesm/localmart/login/LoginScreen.kt
+++ localmart/app/src/main/java/mx/itesm/localmart/login/LoginScreen.kt
@@ -1,7 +1,10 @@
 package mx.itesm.localmart.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login_screen.*
+import mx.itesm.localmart.MainActivity
 import mx.itesm.localmart.R
 
 class LoginScreen : AppCompatActivity() {
@@ -9,5 +12,13 @@ class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+
+        button.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        textView2.setOnClickListener{
+            startActivity(Intent(this, SignupScreen::class.java))
+        }
     }
 }
