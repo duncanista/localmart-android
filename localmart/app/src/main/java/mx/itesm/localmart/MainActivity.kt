39abb4523682f7d7131434b3b1fe4382141b6ac6
Duncanista <30836115+duncanista@users.noreply.github.com>
2020-05-13 06:11:31 -0500

    Toolbar and BottomNavBar style changes

--- localmart/app/src/main/java/mx/itesm/localmart/MainActivity.kt
+++ localmart/app/src/main/java/mx/itesm/localmart/MainActivity.kt
@@ -3,6 +3,7 @@ package mx.itesm.localmart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,12 +11,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView = bottom_navigation
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
 
