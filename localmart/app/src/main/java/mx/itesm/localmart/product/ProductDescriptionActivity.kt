0a9ffa7bbfe96641be3ad7aaa28f34fe8d975a4b
joaquinrios <30807767+joaquinrios@users.noreply.github.com>
2020-05-13 14:54:16 -0500

    ProductDescription
    Contact Info now displays on ProductDescription screen.

--- localmart/app/src/main/java/mx/itesm/localmart/product/ProductDescriptionActivity.kt
+++ localmart/app/src/main/java/mx/itesm/localmart/product/ProductDescriptionActivity.kt
@@ -1,16 +1,26 @@
 package mx.itesm.localmart.product
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_product_description.*
 import kotlinx.android.synthetic.main.product_list_item.view.*
+import mx.itesm.localmart.ListenerRecycler
 import mx.itesm.localmart.R
+import mx.itesm.localmart.auth.Auth
 
 class ProductDescriptionActivity : AppCompatActivity() {
 
+    //Firebase
+    private var firestore = FirebaseFirestore.getInstance()
+    private val Auth: Auth = Auth()
+
+    internal var filepath: Uri? = null
     var storage = FirebaseStorage.getInstance()
+    var storageReference = storage!!.reference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,5 +41,16 @@ class ProductDescriptionActivity : AppCompatActivity() {
                 .load(imgUrl)
                 .into(imgView)
         }
+
+        firestore.collection("users").document(product?.seller).get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val document = task.result
+                    tvSellerName.text = document!!.data!!["name"].toString()
+                    tvSellerEmail.text = document!!.data!!["email"].toString()
+                    tvSellerPhoneNumber.text = document!!.data!!["phone"].toString()
+                    }
+            }
+
     }
 }
