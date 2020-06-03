aeec36fddc0dac0aec68f1f41ff5896aec8ab89e
joaquinrios <30807767+joaquinrios@users.noreply.github.com>
2020-06-03 13:35:17 -0500

    Contact button
    Contact button now takes you to phone app.

--- localmart/app/src/main/java/mx/itesm/localmart/product/ProductDescriptionActivity.kt
+++ localmart/app/src/main/java/mx/itesm/localmart/product/ProductDescriptionActivity.kt
@@ -1,16 +1,16 @@
 package mx.itesm.localmart.product
 
+import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_product_description.*
-import kotlinx.android.synthetic.main.product_list_item.view.*
-import mx.itesm.localmart.ListenerRecycler
 import mx.itesm.localmart.R
 import mx.itesm.localmart.auth.Auth
+
 
 class ProductDescriptionActivity : AppCompatActivity() {
 
@@ -21,6 +21,8 @@ class ProductDescriptionActivity : AppCompatActivity() {
     internal var filepath: Uri? = null
     var storage = FirebaseStorage.getInstance()
     var storageReference = storage!!.reference
+
+    var phoneNumber: String = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,8 +53,16 @@ class ProductDescriptionActivity : AppCompatActivity() {
                     tvSellerName.text = document!!.data!!["name"].toString()
                     tvSellerEmail.text = document!!.data!!["email"].toString()
                     tvSellerPhoneNumber.text = document!!.data!!["phone"].toString()
+                    phoneNumber = document!!.data!!["phone"].toString()
                     }
             }
+
+        btnContactSeller.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            phoneNumber = "tel:$phoneNumber"
+            intent.data = Uri.parse(phoneNumber)
+            startActivity(intent)
+        }
 
     }
 }
